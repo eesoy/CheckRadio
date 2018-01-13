3559f2a9a165ffde1d4ef666f25baf19a4aec4f7
SO YOUNG <LEESOYOUNG@itui-macbook-pro.local>
2018-01-13 17:37:01 +0900

    radio button open source

--- CheckRadio/ViewController.swift
+++ CheckRadio/ViewController.swift
@@ -10,10 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btn_check: DLRadioButton!
+    @IBOutlet weak var btn_radio1: DLRadioButton!
+    @IBOutlet weak var btn_radio2: DLRadioButton!
+    @IBOutlet weak var btn_radio3: DLRadioButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        btn_check.isMultipleSelectionEnabled = true
+        btn_radio1.isEnabled = false
+        btn_radio2.isEnabled = false
+        btn_radio3.isEnabled = false
+        
+
     }
+    
+    @IBAction func startClicked(_ sender: DLRadioButton) {
+        if btn_check.isSelected {
+            btn_radio1.isEnabled = true
+            btn_radio2.isEnabled = true
+            btn_radio3.isEnabled = true
+        } else {
+            btn_radio1.isEnabled = false
+            btn_radio2.isEnabled = false
+            btn_radio3.isEnabled = false
+        }
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
